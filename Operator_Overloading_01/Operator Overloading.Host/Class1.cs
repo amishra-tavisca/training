6a94f4aab3f0ef3172e40fed2bc905764da30695
Abhishek Mishra <amishra@tavisca.com>
2015-07-09 13:14:33 +0530

    Committing changes to push.

---
+++ Operator_Overloading_01/Operator Overloading.Host/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+
+namespace OperatorOverloading.exception
+{
+
+    [Serializable]
+    public class CurrencyNotMatch : Exception
+    {
+
+        public CurrencyNotMatch()
+            : base()
+        {
+
+        }
+
+        public CurrencyNotMatch(string message)
+            : base(message)
+        {
+        }
+
+        public CurrencyNotMatch(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
