7d8ff23237cc86d0e85b2f049fd70bc3523f4b35
Abhishek Mishra <amishra@tavisca.com>
2015-07-21 16:50:11 +0530

    commited custom string

---
+++ CustomString/javaScript.js
@@ -0,0 +1,46 @@
+String.prototype.Concat = function ()
+{
+    var firstString = this;
+    var result = "";
+    var index1 = 0;
+    for (var count = 0; count < arguments.length; count++)
+    {
+        var secondString = arguments[count];
+
+        if (typeof (secondString) == null || typeof (secondString) == undefined) {
+            console.log("not a proper string !");
+            return;
+        }
+
+        //var firstString = this;
+        //var result = "";
+        //var index1 = 0;
+        if (count == 0) {
+            for (index1 = 0; index1 < firstString.length; index1++)
+                result += firstString.charAt(index1);
+        }
+        for (index1 = 0; index1 < secondString.length; index1++)
+            result += secondString.charAt(index1);
+        
+    }
+    return(result);
+
+}
+
+String.prototype.SubString = function (startIndex, endIndex) 
+{
+   var firstString = this;
+   if ((startIndex > endIndex) || endIndex >= this.length) 
+   {
+       console.log("Invalid Index");
+       return;
+   }
+   var result = "";
+   if (startIndex > endIndex || startIndex > firstString.length || endIndex > firstString.length)
+       console.log("invalid index");
+   for (; startIndex < endIndex; startIndex++)
+   {
+       result += firstString.charAt(startIndex);
+   }
+   return(result);
+}
